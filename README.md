dc6dab2f8972cee60472c788a302719229834b46
Brian <bmla.pt@gmail.com>
2023-09-06 01:57:12 -0700

    Models, Technician(get,post,delete) done

--- README.md
+++ README.md
@@ -12,6 +12,16 @@ Team:
 Explain your models and integration with the inventory
 microservice, here.
 
+Purpose of this microservice is to keep track of service appointments for automobiles and their owners.
+
+3 Models:
+Technician:
+AutomobileVO: will poll the 'vin' and 'sold' fields from Automobile model in inventory and will be used for the special feature.
+Appointment:
+
+Special Feature:
+If the VIN in the appointment matches a VIN from the AutomobileVO, then it means that it was sold through us and are considered a VIP.
+
 ## Sales microservice
 
 Explain your models and integration with the inventory
--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Technician(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    employee_id = models.CharField(max_length=200, unique=True)
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=200, unique=True)
+    sold = models.BooleanField()
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    reason = models.CharField(max_length=200)
+    status = models.CharField(max_length=15)
+    vin = models.CharField(max_length=200) #this VIN isn't unique because the person has to type it in, so if they make multiple appointments, then this VIN might be re-used
+    technician = models.ForeignKey(
+        Technician,
+        related_name = "appointment",
+        on_delete=models.CASCADE
+    )
