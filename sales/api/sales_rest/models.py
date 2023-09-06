75db8b0eda0f3ecf572489eb38864b28c9286cf2
emily yim <emilyyim@MacBook-Pro.local>
2023-09-06 10:09:02 -0700

    created models for salesperson, customer, sale, and automobileVO

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
+
 # Create your models here.
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+    sold = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.vin
+
+
+class Salesperson(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Sale(models.Model):
+    price = models.PositiveSmallIntegerField()
+
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="automobile",
+        on_delete=models.CASCADE
+    )
+
+    salesperson = models.ForeignKey(
+        Salesperson,
+        related_name="salesperson",
+        on_delete=models.PROTECT
+    )
+
+    customer = models.ForeignKey(
+        Customer,
+        related_name="customer",
+        on_delete=models.PROTECT
+    )
