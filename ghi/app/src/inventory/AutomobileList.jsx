437ae8286075b41595d19b19e73bc3092dce92f8
Brian <bmla.pt@gmail.com>
2023-09-05 17:57:06 -0700

    automobile list complete

--- ghi/app/src/inventory/AutomobileList.jsx
+++ ghi/app/src/inventory/AutomobileList.jsx
@@ -2,91 +2,60 @@ import { NavLink } from 'react-router-dom';
 import React, {useEffect, useState} from "react";
 import '../index.css'
 
-const AutomobileList = (props) => {
-    console.log('list of automobiles')
-    // // This code is to get and show the shoe data in a list
-    // // console.log(props)
-    // const [shoes, setShoes] = useState([]);
-    // const [shoe, setShoe] = useState('');
-    // const fetchData = async () => {
-    //     const allShoesUrl = `http://localhost:8080/api/bins/shoes/`;
+const AutomobileList = () => {
+    // console.log('list of automobiles')
+    const [autos, setAutos] = useState([]);
+    const fetchData = async () => {
+        const autosUrl = `http://localhost:8100/api/automobiles/`;
 
-    //     try {
-    //         const shoesResponse = await fetch(allShoesUrl);
-    //         if (shoesResponse.ok) {
-    //             const shoesData = await shoesResponse.json();
-    //             // console.log(shoesData.shoes)
-    //             const request = [];
-    //             const shoeList = shoesData.shoes;
-    //             setShoes(shoeList)
-    //         }
-    //     } catch (e) {
-    //         console.error(e)
-    //     }
+        try {
+            const autosResponse = await fetch(autosUrl);
+            if (autosResponse.ok) {
+                const autosData = await autosResponse.json();
+                // console.log(autosData.autos)
+                const autosList = autosData.autos;
+                setAutos(autosList)
+            }
+        } catch (e) {
+            console.error(e)
+        }
+    }
 
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-    // }
+    return (
+        <div>
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>VIN</th>
+                        <th>Color</th>
+                        <th>Year</th>
+                        <th>Model</th>
+                        <th>Manufacturer</th>
+                        <th>Sold</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {autos.map(auto => {
+                    return (
+                        <tr key = { auto.vin }>
+                            <td>{ auto.vin }</td>
+                            <td>{ auto.color }</td>
+                            <td>{ auto.year } </td>
+                            <td>{ auto.model.manufacturer.name }</td>
+                            <td>{ auto.model.name }</td>
+                            <td>{ auto.sold ? "Yes" : "No" }</td>
+                        </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
 
-    // useEffect(() => {
-    //     fetchData();
-    // }, []);
-    // // console.log(shoes[1].href)
-
-    // const handleDelete = async (href) => {
-    //     // const shoeUrl = `http://localhost:8080/${shoes}`
-    //     const newShoeList = shoes.filter((shoe) => shoe.href != href);
-    //     console.log(newShoeList);
-    //     const url = `http://localhost:8080${href}`;
-    //     const deleteOptions = {
-    //         method: 'DELETE',
-    //         headers: {
-    //         'Content-Type': 'application/json'
-    //         },
-    //         body: null
-    //     }
-    //     const deleteResponse = await fetch(url, deleteOptions);
-    //     if (deleteResponse.ok) {
-    //         setShoes(newShoeList);
-    //     }
-    //     console.log(newShoeList);
-    // }
-
-    // return (
-    //     <div>
-    //         <NavLink className="nav-link" to="/shoes/new">Add Shoes</NavLink>
-    //         <table className="table table-striped table-hover">
-    //             <thead>
-    //                 <tr>
-    //                     <th>Picture</th>
-    //                     <th>Manufacturer</th>
-    //                     <th>Model Name</th>
-    //                     <th>Color</th>
-    //                     <th>Bin Number</th>
-    //                     <th>Closet Name</th>
-    //                     <th>Action</th>
-    //                 </tr>
-    //             </thead>
-    //             <tbody>
-    //                 {shoes.map(shoe => {
-    //                 return (
-    //                     <tr key = { shoe.href }>
-    //                 <td><img src={ shoe.picture_url } /></td>
-    //                 <td>{ shoe.manufacturer }</td>
-    //                 <td>{ shoe.model_name }</td>
-    //                 <td>{ shoe.color }</td>
-    //                 <td>{ shoe.bin.number }</td>
-    //                 <td>{ shoe.bin.closet_name } </td>
-    //                 <td>
-    //                     <button className='delete' type='button' onClick={() => handleDelete(shoe.href)}> Delete </button>
-    //                 </td>
-    //                 </tr>
-    //                 );
-    //             })}
-    //             </tbody>
-    //         </table>
-    //     </div>
-
-    // );
+    );
 }
 
 export default AutomobileList;
