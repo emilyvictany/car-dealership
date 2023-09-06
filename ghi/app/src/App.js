2d93775264940059ebce7211ad1d8fd42c51ec50
Brian <bmla.pt@gmail.com>
2023-09-05 17:24:49 -0700

    nav bar front end created

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import AutomobileList from './inventory/AutomobileList';
+import ModelForm from './inventory/CreateModel';
+import AutomobileForm from './inventory/CreateAutomobile';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/model/new" element={<ModelForm />} />
+          <Route path="/automobiles" element={<AutomobileList />} />
+          <Route path="/automobiles/new" element={<AutomobileForm />} />
         </Routes>
       </div>
     </BrowserRouter>
