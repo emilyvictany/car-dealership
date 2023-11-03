0e253721fc7aab0d25388045d8a1bc32dbe237c2
emily yim <emilyyim@MacBook-Pro.local>
2023-11-03 13:27:36 -0700

    better navbar formatting

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,66 +1,65 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import React from "react"
 
 function Nav() {
   return (
+    <nav className="navbar navbar-start navbar-expand-lg navbar-dark" style={{ backgroundImage: "linear-gradient(140deg, #AB400C 0%, #ED8B16 50%, #E8E0D1 75%)" }}>
     <div>
-      <nav className="navbar navbar-start navbar-expand-lg navbar-dark" style={{ backgroundImage: "linear-gradient(140deg, #AB400C 0%, #ED8B16 50%, #E8E0D1 75%)" }}>
-        <div className="container-fluid flex-1" >
-          <NavLink className="navbar-brand btn btn-ghost normal-case text-xl" to="/">Sunset Motors</NavLink>
-        </div>
-        <div className="collapse navbar-collapse justify-content-center" >
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <NavLink className="navbar-brand btn fw-bold" to="/">Sunset Motors</NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-          <div className="dropdown">
-            <details className="dropdown">
-              <summary className="m-1 btn">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item dropdown me-2">
+              <NavLink className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Cars
-              </summary>
-              <div className="p-2 shadow menu-vertical bg-base-100 rounded-box w-52">
-                <div><NavLink className="nav-link" to="/manufacturers">Manufacturers</NavLink></div>
-                <div><NavLink className="nav-link" to="/manufacturer/create">Create a Manufacturer</NavLink></div>
-                <div><NavLink className="nav-link" to="/models">Models</NavLink></div>
-                <div><NavLink className="nav-link" to="/model/new">Create a Model</NavLink></div>
-                <div><NavLink className="nav-link" to="/automobiles">Automobiles</NavLink></div>
-                <div><NavLink className="nav-link" to="/automobile/new">Create an Automobile</NavLink></div>
-              </div>
-            </details>
-          </div>
+              </NavLink>
+              <ul className="dropdown-menu dropdown-menu-end">
+                <li><Link className="nav-link" to="/manufacturers">Manufacturers</Link></li>
+                <li><Link className="nav-link" to="/manufacturer/create">Create a Manufacturer</Link></li>
+                <li><Link className="nav-link" to="/models">Models</Link></li>
+                <li><Link className="nav-link" to="/model/new">Create a Model</Link></li>
+                <li><Link className="nav-link" to="/automobiles">Automobiles</Link></li>
+                <li><Link className="nav-link" to="/automobile/new">Create an Automobile</Link></li>
+              </ul>
+            </li>
 
-          <div className="dropdown">
-            <details className="flex-col space-y-2">
-              <summary className="m-1 btn">
+            <li className="nav-item dropdown me-2">
+              <NavLink className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Sales
-              </summary>
-              <div className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                <div><NavLink className="nav-link" to="/customer/create">Add a Customer</NavLink></div>
-                <div><NavLink className="nav-link" to="/customers">Customers</NavLink></div>
-                <div><NavLink className="nav-link" to="/sale/create">Record a New Sale</NavLink></div>
-                <div><NavLink className="nav-link" to="/sales">Sales</NavLink></div>
-                <div><NavLink className="nav-link" to="/salesperson/list">Salespeople</NavLink></div>
-                <div><NavLink className="nav-link" to="/salesperson/create">Add a Salesperson</NavLink></div>
-                <div><NavLink className="nav-link" to="/salesperson/history">Salesperson History</NavLink></div>
-              </div>
-            </details>
-          </div>
+              </NavLink>
+              <ul className="dropdown-menu dropdown-menu-end">
+                <li><Link className="nav-link" to="/customer/create">Add a Customer</Link></li>
+                <li><Link className="nav-link" to="/customers">Customers</Link></li>
+                <li><Link className="nav-link" to="/sale/create">Record a New Sale</Link></li>
+                <li><Link className="nav-link" to="/sales">Sales</Link></li>
+                <li><Link className="nav-link" to="/salesperson/list">Salespeople</Link></li>
+                <li><Link className="nav-link" to="/salesperson/create">Add a Salesperson</Link></li>
+                <li><Link className="nav-link" to="/salesperson/history">Salesperson History</Link></li>
+              </ul>
+            </li>
 
-          <div className="dropdown">
-            <details className="dropdown menu">
-              <summary className="m-1 btn">
+            <li className="nav-item dropdown me-2">
+              <NavLink className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Services
-              </summary>
-              <div className="menu flex-col space-y-2 dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                <div><NavLink className="nav-link" to="/appointment/new">Create Appointment</NavLink></div>
-                <div><NavLink className="nav-link" to="/appointments">Service Appointments</NavLink></div>
-                <div><NavLink className="nav-link" to="/appointments/history">Service History</NavLink></div>
-                <div><NavLink className="nav-link" to="/technician/new">Create Technician</NavLink></div>
-                <div><NavLink className="nav-link" to="/technicians">Technicians</NavLink></div>
-              </div>
-            </details>
-          </div>
-
+              </NavLink>
+              <ul className="dropdown-menu dropdown-menu-end">
+                <li><Link className="nav-link" to="/appointment/new">Create Appointment</Link></li>
+                <li><Link className="nav-link" to="/appointments">Service Appointments</Link></li>
+                <li><Link className="nav-link" to="/appointments/history">Service History</Link></li>
+                <li><Link className="nav-link" to="/technician/new">Create Technician</Link></li>
+                <li><Link className="nav-link" to="/technicians">Technicians</Link></li>
+              </ul>
+            </li>
+          </ul>
         </div>
-      </nav>
+
+      </div>
     </div>
+    </nav>
   )
 }
 
